a53e4eedba61594407abd351cbb84f5be3f5b179
bingxuanying <yingbingxuan@gmail.com>
2021-12-20 23:27:26 -0800

    Add vagrantfile and consumer

---
+++ src/producers/producer_amzn.py
@@ -0,0 +1,54 @@
+#
+#
+# Author: Aniruddha Gokhale
+# CS4287-5287: Principles of Cloud Computing, Vanderbilt University
+#
+# Created: Sept 6, 2020
+#
+# Purpose:
+#
+#    Demonstrate the use of Kafka Python streaming APIs.
+#    In this example, we use the "top" command and use it as producer of events for
+#    Kafka. The consumer can be another Python program that reads and dumps the
+#    information into a database OR just keeps displaying the incoming events on the
+#    command line consumer (or consumers)
+#
+
+import time # for sleep
+from kafka import KafkaProducer  # producer of events
+from yahoo_fin import stock_info as si
+from datetime import datetime
+
+# We can make this more sophisticated/elegant but for now it is just
+# hardcoded to the setup I have on my local VMs
+
+# acquire the producer
+# (you will need to change this to your bootstrap server's IP addr)
+producer = KafkaProducer (bootstrap_servers="52.91.235.17:9092", 
+                                          acks=1)  # wait for leader to write to log
+
+# say we send the contents 100 times after a sleep of 1 sec in between
+for i in range (100):
+
+    amz_stock_price = si.get_live_price("amzn")
+
+    now = datetime.now()
+
+    contents = "AMZN " + str(amz_stock_price) + " at " + now.strftime("%m/%d/%YT%H:%M:%S")
+
+    # send the contents under topic utilizations. Note that it expects
+    # the contents in bytes so we convert it to bytes.
+    #
+    # Note that here I am not serializing the contents into JSON or anything
+    # as such but just taking the output as received and sending it as bytes
+    # You will need to modify it to send a JSON structure, say something
+    # like <timestamp, contents of top>
+    #
+    producer.send ("utilizations", value=bytes (contents, 'ascii'))
+    producer.flush ()   # try to empty the sending buffer
+
+    # sleep a second
+    time.sleep (1)
+
+# we are done
+producer.close ()
